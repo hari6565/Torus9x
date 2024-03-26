e9ec5663fe9251a05e6a134571bf0a8b48bd4a11
hari6565 <harin@torus.tech>
2024-03-26 13:45:58 +0530

    keycloak

---
+++ app/(Flow)/Flow/loading.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+import { Card, Skeleton } from "@nextui-org/react";
+
+export default function Loading() {
+  return (
+    <div className="flex flex-col w-full h-screen p-8 gap-3">
+      <Skeleton className="rounded-lg h-3/4">
+        <div className="h-24 rounded-lg bg-default-300"></div>
+      </Skeleton>
+      <div className="space-y-3">
+        <Skeleton className="w-3/5 rounded-lg">
+          <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+        </Skeleton>
+        <Skeleton className="w-4/5 rounded-lg">
+          <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+        </Skeleton>
+        <Skeleton className="w-2/5 rounded-lg">
+          <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
+        </Skeleton>
+      </div>
+    </div>
+  );
+}
